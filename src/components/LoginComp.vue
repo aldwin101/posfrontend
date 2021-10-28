b38b9f230c65a81a0d447240075e49c2b29d1269
Aldwin <aldwin828@gmail.com>
2021-10-28 00:07:07 -0400

    Added login page

---
+++ src/components/LoginComp.vue
@@ -0,0 +1,95 @@
+<template>
+    <div id="container">
+        <div id="loginContainer">
+            <input type="text" v-model="username">
+            <input type="password" v-model="password">
+            <button type="submit" @click="login">Submit</button>
+        </div>
+        <div id="logoContainer">
+            <img src="@/assets/POSLogo.png" alt="logo">
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+// import cookies from "vue-cookies"
+    export default {
+        name : "LoginComp",
+        data() {
+            return {
+                pin : ""
+            }
+        },
+        methods: {
+            login(){
+                axios.request({
+                    url: process.env.VUE_APP_BASE_DOMAIN+"api/login",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    data: {
+                        "userPin" : this.pin
+                    }
+                }).then((response) => {
+                    console.log("Success");
+                    console.log(response.data.loginToken);
+                    console.log(response.data.userId);
+                    // cookies.set('token', response.data.loginToken);
+                    // cookies.set('userLoggedinId', response.data.userId);
+                }).catch((error) => {
+                    console.log("Failed");
+                    console.log(this.pin);
+                    console.log(error.response);
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+#container {
+    background-color: transparent;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+    justify-items: center;
+    align-items: center;
+}
+
+#logoContainer{
+    display: grid;
+    align-items: center;
+    background-color: #d3c1ab;
+    height: 100vh;
+    width: auto;
+}
+
+input {
+    background-color: #f0f8ff;
+    border-radius: 2vw;
+    border-color:transparent;
+    width: 30vw;
+    height: 5vh;
+    font-size: 2vw;
+    text-align: center;
+}
+
+button {
+    width: 15vw;
+    font-weight: bold;
+    background-color: rgb(57, 104, 145);
+    color: #f0f8ff;
+    font-size: 2vw;
+    border-radius: 1vw;
+}
+
+#loginContainer {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    row-gap: 2vw;
+    justify-items: center;
+}
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <LoginComp />
+    </div>
+</template>
+
+<script>
+import LoginComp from "../components/LoginComp.vue"
+    export default {
+        name : "Login",
+        components: {
+            LoginComp,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
